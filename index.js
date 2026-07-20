e703b6cbb66e55f07f08105c99b8ba40c352848b
RefiqeRhml <rhml.7r@gmail.com>
2026-07-20 19:01:41 +0400

    hw20

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(Math.floor(Math.random() * (100 - 95 + 1) + 95))
+let fullName = "Rehimli Refiqe";
+let words = fullName.split(" ");
+console.log(words[1] + " " + words[0]);
+
+console.log(words[0][0] + " " + words[1][0]);
+
+let word = fullName.replaceAll(" ", "");
+console.log(word.length)
