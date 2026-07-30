528c573ad171ecb06eb815a5c9af7f92bc168161
RefiqeRhml <rhml.7r@gmail.com>
2026-07-30 16:33:17 +0400

    hw23

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-let a = 12345;
-let b = 98765;
-console.log(String(c = a * b)[5]);
+let month = prompt("Ayin nomresin daxil edin:");
+switch (month) {
+    case "01":
+        alert("January");
+        break;
+    case "02":
+        alert("February");
+        break;
+    case "03":
+        alert("March");
+        break;
+    case "04":
+        alert("April");
+        break;
+    case "05":
+        alert("May");
+        break;
+    case "06":
+        alert("June");
+        break;
+    case "07":
+        alert("July");
+        break;
+    case "08":
+        alert("August");
+        break;
+    case "09":
+        alert("September");
+        break;
+    case "10":
+        alert("October");
+        break;
+    case "11":
+        alert("November");
+        break;
+    case "12":
+        alert("December");
+        break;
+    default:
+        alert("Sehv nomre")
+}
