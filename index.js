50e428420f658ee9f62c687b1c50d2f377d0b887
RefiqeRhml <rhml.7r@gmail.com>
2026-08-03 18:52:23 +0400

    hw24

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-let month = prompt("Ayin nomresin daxil edin:");
-switch (month) {
-    case "01":
-        alert("January");
-        break;
-    case "02":
-        alert("February");
-        break;
-    case "03":
-        alert("March");
-        break;
-    case "04":
-        alert("April");
-        break;
-    case "05":
-        alert("May");
-        break;
-    case "06":
-        alert("June");
-        break;
-    case "07":
-        alert("July");
-        break;
-    case "08":
-        alert("August");
-        break;
-    case "09":
-        alert("September");
-        break;
-    case "10":
-        alert("October");
-        break;
-    case "11":
-        alert("November");
-        break;
-    case "12":
-        alert("December");
-        break;
-    default:
-        alert("Sehv nomre")
+function user(username) {
+  console.log(`${username} is active now`);
 }
+user("Alex");
+user("Sarah");
+user("John");
+
+function num(a, b, c) {
+  const max = Math.max(a, b, c);
+  console.log(max);
+}
+num(10, 45, 23);
