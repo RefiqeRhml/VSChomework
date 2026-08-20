ad096dc4df5de7172be69ac908c6d9e527c7847a
RefiqeRhml <rhml.7r@gmail.com>
2026-08-20 18:40:42 +0400

    hw29

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-function isLucky(number) {
-    let num = String(number);
-    let sum1 = +num[0] + +num[1] + +num[2];
-    let sum2 = +num[3] + +num[4] + +num[5];
-    return sum1 == sum2;
+console.log("task 1")
+
+function word(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].length == 3){
+            return arr[i];
+        }
+    }
 }
-console.log(isLucky(234801));
-console.log(isLucky(236498));
+console.log(word(['Green', 'Red', 'Blue']));
+
+if (name.startsWith('A') || name.startsWith('a'))
+
+console.log("task 2")
+
+let names = ["Alex", "Jane", "John", "Ann", "Abby"];
+let namesA = [];
+names.forEach(name => {
+    if(name.startsWith('A') || ('a')){
+        namesA.push(name);
+    }
+});
+console.log(namesA);
